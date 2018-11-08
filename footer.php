a779127d1581d0ee14d8cfa31f0d6e69a702db50
William Earnhardt <wearnhardt@gmail.com>
2018-11-08 11:47:31 -0500

    Add php 5.4 syntax error

--- footer.php
+++ footer.php
@@ -18,8 +18,7 @@
 	<footer id="colophon" class="site-footer">
 		<?php get_template_part( 'template-parts/footer/footer', 'widgets' ); ?>
 		<div class="site-info">
-			<?php $blog_info = get_bloginfo( 'name' ); ?>
-			<?php if ( ! empty( $blog_info ) ) : ?>
+			<?php if ( ! empty( get_bloginfo( 'name' ) ) ) : ?>
 				<a class="site-name" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>,
 			<?php endif; ?>
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentynineteen' ) ); ?>" class="imprint">
